bcde2e7f0fe99c2efb97572f465fe5d70961348b
YY T <changiyy07@gmail.com>
2024-07-02 15:08:37 +0800

    Color usernames differently

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,10 +4,27 @@ function Message({ message }) {
 
   let displayTime = new Date(message.created_at);
 
+  function hashCode(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  function intToRGB(i){
+    let c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+    return "00000".substring(0, 6 - c.length) + c;
+  }
+
+  let color = intToRGB(hashCode(message.author));
+
   return(
     <div className={styles.messageContainer}>
       <div>
-        <span className={styles.author}>{message.author}</span>
+        <span className={styles.author} style={{ color: `#${color}` }}>{message.author}</span>
         {' - '}
         <span className={styles.timestamp}>{displayTime.toLocaleTimeString()}</span>
       </div>
