e2a4ec9977145a8a17b22491a281073ad7a75856
YY T <changiyy07@gmail.com>
2024-07-02 14:26:49 +0800

    Remove mock data from initialState, messages

--- src/constants/initialState.js
+++ src/constants/initialState.js
@@ -5,17 +5,6 @@ export const initialState = {
     {id: 2, name: 'paris'}
   ],
   selectedChatroomId: 0,
-  messages: [
-    {
-      "author":"anonymous92",
-      "content":"Hello world!",
-      "created_at":"2017-09-26T23:03:16.365Z"
-    },
-    {
-      "author":"anonymous77",
-      "content":"My name is anonymous77",
-      "created_at":"2017-09-26T23:03:21.194Z"
-    }
-  ],
+  messages: [],
   currentUsername: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
 };
