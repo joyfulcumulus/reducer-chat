5404e3f97540a84af0975de4a22f90ac32f864ef
YY T <changiyy07@gmail.com>
2024-07-02 14:55:13 +0800

    Amend autoFocus value from string to boolean

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -29,7 +29,7 @@ function MessageForm({selected, currentUsername}) {
         <input
           type="text" name="content" placeholder="Type a message here"
           value={userInput}
-          autoFocus="true"
+          autoFocus={true}
           onChange={(e) => updateUserInput(e.target.value)}
         />
         <button className={styles.sendButton} onClick={handleSubmit}>Send</button>
